f84dc301eb7963d55df695b740f612f1d1b5774c
tubasthebest <tubasthebest@gmail.com>
2020-11-21 10:53:36 -0700

    added timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,45 @@
-var timerEl = document.getElementById('countdown');
+var timerEl = document.querySelector('#time');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start');
 
 var questions = [
-    
+    {
+        title: "Commonly Used data types DO NOT include:",
+        choices: ["stings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statment is enclosed within _____.",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What javascipt method can we use to select an html element?",
+        choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+        answer: "Both 1 and 3"
+    },
+    {
+        title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+        title: "What attribute is used in html to decorate content?",
+        choices: ["css", "class", "src", "style"],
+        answer: "style"
+    }
 ]
 
 
 
-// Timer that counts down from 5
-function countdown() {
+// Timer that counts down from 75
+function quizStart() {
     var timeLeft = 75;
 
     // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
-        if (timeLeft > 1) {
-            timerEl.textContent = timeLeft + ' seconds remaining';
+        if (timeLeft > 5) {
+            timerEl.textContent = timeLeft;
             timeLeft--;
         }
         else if (timeLeft === 1) {
@@ -49,4 +73,4 @@ function displayMessage() {
     }, 300);
 }
 
-startBtn.onclick = countdown;
+startBtn.onclick = quizStart;
